b384517c277893ab9dbe7446028d90c73aa7fbdb
Bahareh Gholami <bahareh.gh001@gmail.com>
2026-02-25 15:14:29 +0330

    improved

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,8 +2,11 @@ export default function Dashboard () {
 
 
     return(
-        <div className="flex items-center justify-center h-screen">
-            <h1>This page is not yet complete.</h1>
+        <div className="flex flex-col gap-5 items-center justify-center h-screen">
+            <img 
+                src='/under-construction.svg'
+                className="w-170" />
+                <p className="text-[2rem]">Under Construction</p>
         </div>
     )
 }
